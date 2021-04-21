5cb994d35b9bc70cd85e4241c4544374cbc06006
David Alexander Bjerremose <dab@davidbjerremose.com>
2021-04-21 14:35:35 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ echo "Docker Container Building $1:$2"
 
 set -x -e
 
+cp -r $1 /opt/harmony/v2_02_00b/apps/$1
+cp -r src /opt/harmony/v2_02_00b/apps/src
+
 /opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $1@$2 || exit 1
 make -C $1 CONF=$2 build || exit 2
