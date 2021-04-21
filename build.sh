5b95a35b624a06e29c13b6a029db98724531b1a4
David Alexander Bjerremose <dab@davidbjerremose.com>
2021-04-21 15:12:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-HARMONY_PATH=/opt/harmony/v2_02_00b/
-PROJECT_PATH=/
+HARMONY_PATH=/opt/harmony/v2_02_00b
+PROJECT_PATH=""
 
 if [ "$3" -eq "0" ]
   then
     PROJECT_PATH=/
 else
   PROJECT_PATH=$HARMONY_PATH/apps/$3
-  cp -r $1 $PROJECT_PATH$1
+  cp -r $1 $PROJECT_PATH/$1
   cp -r src $PROJECT_PATH/src
 fi
 
@@ -18,5 +18,5 @@ echo "Docker Container Building $1:$2"
 
 set -x -e
 
-/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $PROJECT_PATH$1@$2 || exit 1
-make -C $PROJECT_PATH CONF=$2 build || exit 2
+/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $PROJECT_PATH/$1@$2 || exit 1
+make -C $PROJECT_PATH/$1 CONF=$2 build || exit 2
