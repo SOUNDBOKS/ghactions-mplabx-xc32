a09ad4d7b3fc4fae33089da8983c1cfcee55b969
David Alexander Bjerremose <dab@davidbjerremose.com>
2021-04-21 14:48:14 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+HARMONY_PROJECT_PATH=/opt/harmony/v2_02_00b/apps/$1
+HARMONY_SRC_PATH=/opt/harmony/v2_02_00b/apps/src
+
 echo "Docker Container Building $1:$2"
 
 set -x -e
 
-cp -r $1 /opt/harmony/v2_02_00b/apps/$1
-cp -r src /opt/harmony/v2_02_00b/apps/src
+cp -r $1 $HARMONY_PROJECT_PATH
+cp -r src $HARMONY_SRC_PATH
 
-/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $1@$2 || exit 1
-make -C $1 CONF=$2 build || exit 2
+/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $HARMONY_PROJECT_PATH@$2 || exit 1
+make -C $HARMONY_PROJECT_PATH CONF=$2 build || exit 2
